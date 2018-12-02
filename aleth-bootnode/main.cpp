4cb984dadbd9dcd88d9b94dee8f33d47ca50b948
Nils-Erik Frantzell <nilserik.frantzell@gmail.com>
2018-12-02 13:08:12 -0800

    Add argument processing functionality

--- aleth-bootnode/main.cpp
+++ aleth-bootnode/main.cpp
@@ -19,24 +19,89 @@
 #include <boost/program_options/options_description.hpp>
 #include <libdevcore/LoggingProgramOptions.h>
 #include <libp2p/Host.h>
+#include <iostream>
+#include <thread>
 
 namespace po = boost::program_options;
 
 using namespace dev;
 using namespace dev::p2p;
+using namespace std;
 
 namespace
 {
 unsigned const c_lineWidth = 160;
+
+class ExitHandler
+{
+public:
+    static void exitHandler(int) { s_shouldExit = true; }
+    bool shouldExit() const { return s_shouldExit; }
+
+private:
+    static bool s_shouldExit;
+};
+
+bool ExitHandler::s_shouldExit = false;
 }
 
 int main(int argc, char** argv)
 {
+    setDefaultOrCLocale();
+
+    po::options_description generalOptions("GENERAL OPTIONS", c_lineWidth);
+    auto addGeneralOption = generalOptions.add_options();
+    addGeneralOption("help,h", "Show this help message and exit\n");
+
     LoggingOptions loggingOptions;
      po::options_description loggingProgramOptions(
         createLoggingProgramOptions(c_lineWidth, loggingOptions));
     
+    po::options_description allowedOptions("Allowed options");
+    allowedOptions.add(generalOptions);
+    allowedOptions.add(loggingProgramOptions);
+    po::variables_map vm;
+    try
+    {
+        po::parsed_options parsed = po::parse_command_line(argc, argv, allowedOptions);
+        po::store(parsed, vm);
+        po::notify(vm);
+    }
+    catch (po::error const& e)
+    {
+        cout << e.what() << endl;
+        return -1;
+    }
+    
+    if (vm.count("help"))
+    {
+        cout << "NAME:\n"
+             << "   aleth-bootnode\n"
+             << "USAGE:\n"
+             << "   aleth-bootnode [options]\n\n";
+        cout << generalOptions << loggingProgramOptions;
+        return 0;
+    }
+
+    setupLogging(loggingOptions);
+
+    if (loggingOptions.verbosity > 0)
+        cout << EthGrayBold "aleth-bootnode, a C++ Ethereum bootnode implementation" EthReset << "\n";
+
     Host h("aleth-bootnode");
+    h.start();
+
+    cout << "Node ID: " << h.enode() << endl;
+
+    ExitHandler exitHandler;
+    signal(SIGTERM, &ExitHandler::exitHandler);
+    signal(SIGABRT, &ExitHandler::exitHandler);
+    signal(SIGINT, &ExitHandler::exitHandler);
+   
+    while (!exitHandler.shouldExit())
+        this_thread::sleep_for(chrono::milliseconds(1000));
+
+    h.stop();
 
     return 0;
 }
